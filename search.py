6f07888897a6191dcc3bdb53cb4143d9067993b5
Arnas Danaitis <arnas.danaitis@gmail.com>
2018-10-29 17:22:05 +0200

    diversified method.

--- search.py
+++ search.py
@@ -2,13 +2,35 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-media_html = requests.get('https://www.google.com/search?q=testas').text
-soup = BeautifulSoup(media_html, 'lxml')
-results_block = soup.find("div", id="ires")
-for result in results_block.find_all("div", class_="g"):
-    print(result.find("a").get('href').split("://")[1].split('/')[0])
-    print(result.find("h3").get_text())
-    for word in result.find_all("b"):
-        if (word.string != "..."):
-            print(word.string)
+class Result:
+    title: str
+    link: str
+    words: list
+    def __init__(self, title, link, words):
+        self.title = title
+        self.link = link
+        self.words = words
+
+def get_results(query: str):
+    results = []
+    media_html = requests.get('https://www.google.com/search?q=' + query.replace(" ", "+")).text
+    soup = BeautifulSoup(media_html, 'lxml')
+    results_block = soup.find("div", id="ires")
+    for result in results_block.find_all("div", class_="g"):
+        link = result.find("a").get('href')
+        if ("http" not in link):
+            continue
+        new_result = Result(title=result.find("h3").get_text(),
+                            link=link.split("://")[1].split('/')[0],
+                            words=[])
+        for word in result.find_all("b"):
+            if (word.string != "..."):
+                new_result.words.append(word.string)
+        results.append(new_result)
+    return results
+
+for result in get_results("test"):
+    print(result.title)
+    print(result.link)
+    print(result.words)
     print('\n')
