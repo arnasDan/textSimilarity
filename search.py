47349d93bfa47270048b4cf40b12278935126513
Danaitis <arnas.danaitis@gmail.com>
2018-10-31 13:13:26 +0200

    GUI

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ from nltk import tokenize
 import docx2txt
 import PySimpleGUI as gui
 import ctypes
+import textwrap
 
 class Result:
     title: str
@@ -13,6 +14,11 @@ class Result:
         self.title = title
         self.link = link
         self.words = words
+    def contains_word(self, new_word):
+        for word in self.words:
+            if word.lower() == new_word.lower():
+                return True
+        return False
 
 def get_results(query: str):
     results = []
@@ -27,7 +33,7 @@ def get_results(query: str):
                             link=link.split("://")[1].split('/')[0],
                             words=[])
         for word in result.find_all("b"):
-            if word.string != "...":
+            if word.string != "..." and not new_result.contains_word(word.string):
                 new_result.words.append(word.string)
         results.append(new_result)
     return results
@@ -51,7 +57,7 @@ def split_text(text: str):
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 filename = gui.PopupGetFile("Choose a document to check:", file_types=(("Word documents (*.docx)", "*.docx"),))
-if (filename is None):
+if (filename == None):
     exit()
 
 try:
@@ -66,23 +72,22 @@ except PermissionError:
 results = dict()
 source_chunks = split_text(document_text)
 for i, chunk in enumerate(source_chunks):
-    gui.OneLineProgressMeter('Executing searches...', i + 1, len(source_chunks), 'key')
+    if not gui.OneLineProgressMeter('Executing searches...', i + 1, len(source_chunks), 'search_meter') and i + 1 < len(source_chunks):
+        exit()
     results[chunk] = get_results(chunk)
 
-gui.SetOptions(element_padding=(0,0))
-column_layout = [[]]
-width = 30 #round(max(len(result.source_chunk) for result in results.values()) / 8) + 10
+gui.SetOptions(element_padding=(0, 0))
+layout = [[gui.T('Results:', font='Any 18')]]
 for result in results.items():
-    column_layout.append([gui.Multiline(result[0])])
-    column_layout.append([
-        (gui.T(
-            res.title + '\n' + res.link + '\n' + '; '.join(res.words),
-            background_color='white',
-            pad=(1,1),
-            size=(32, 1))
-        ) for res in result[1]
-    ])
-layout = [  [gui.T('Results:', font='Any 18')],
-            [gui.Column(column_layout, size=(800,600), scrollable=True)] ]
-window = gui.Window('Search results').Layout(layout)
+    column_layout = [[gui.T('No matches', background_color='white', pad=(1,1))]] if len(result[1]) == 0 else [[]]
+    for values in result[1]:
+        column_layout.append([
+            (gui.T(textwrap.fill(values.title, 55) + '\n' + values.link, background_color='white', pad=(1,1), size=(55, 3))),                        
+            (gui.T(textwrap.fill('; '.join(values.words), 40), background_color='white', pad=(1,1), size=(40, 3)))
+        ])
+    header_rows = textwrap.wrap(result[0], 100)
+    layout.extend([[gui.T('\n' + '\n'.join(header_rows), size=(120, len(header_rows) + 1), font='Bold')],
+                   [gui.Column(column_layout)]])
+wrapper = [[gui.Column(layout, scrollable=True, size=(920, 700))]]
+window = gui.Window('Search results').Layout(wrapper)
 window.Show()
