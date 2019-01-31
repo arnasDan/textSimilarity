791126ad64fdf649f662d1c9c4bac540cf6d10f5
Arnas Danaitis <arnas.danaitis@gmail.com>
2019-01-31 22:05:53 +0200

    Office.js add-in groundwork

---
+++ pages/Scripts/Home.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+(function () {
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            if (Office.context.requirements.isSetSupported('WordApi', 1.1)) {
+                $('#processSelection').click(processSelection);
+                $('#supportedVersion').html('This code is using Word 2016 or later.');
+            }
+            else {
+                $('#supportedVersion').html('This code requires Word 2016 or later.');
+            }
+        });
+    };
+
+    function processSelection() {
+        var text;
+        Word.run(function (context) {
+            var range = context.document.getSelection();
+            range.load('text');
+            context.sync()
+                .then(function () {
+                    //$('#content-main').append(range.text);
+                    text = range.text;
+                    console.log(range.text + " test");
+                    $.ajax({
+                        url: "process_text",
+                        data: {
+                            text: range.text
+                        },
+                        type: "GET",
+                        dataType: "html",
+                        success: function(results) {
+                            $('#content-main').append(results);
+                        },
+                        error: function(xhr, status) {
+                            console.log(status);
+                        }
+                        
+                        
+                    });
+                });
+        });
+    };
+})();
